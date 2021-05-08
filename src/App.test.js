833e0431e01a9fdae4b37cc399cafb8917d0b47b
sj122508gh <jamesdviojan@gmail.com>
2021-05-08 18:42:21 +0800

    Initial-push-for-test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react'
+import Enzyme, {shallow}from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({adapter: new EnzymeAdapter()});
+// import ReactDOM from 'react-dom'
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders without crashing', () => {
+ const wrapper = shallow(<App />)
+//  console.log(wrapper.debug())
+ expect(wrapper).toBeTruthy();
+})
