e18f96dceabd758d14643726719b725ffa46d57b
James Viojan <james.viojan@accenture.com>
2021-05-10 15:47:39 +0800

    Add Prototypes for Congrats componen

--- test/testUtilities.js
+++ test/testUtilities.js
@@ -1,4 +1,4 @@
-// import { ShallowWrapper } from "enzyme";
+import checkPropTypes from 'check-prop-types'
 
 /**
  * Return  node(s) with the given data-test attribute.
@@ -8,4 +8,14 @@
  */
 export const findByTestAttribute = (wrapper, value) => {
     return wrapper.find(`[data-test="${value}"]`);
+}
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes, 
+        conformingProps,
+        'prop', 
+        component.name)
+
+    expect(propError).toBeUndefined();
 }
