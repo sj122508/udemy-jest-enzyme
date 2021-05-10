9a990651a53f833e9cb2ce903403367a1baca29f
James Viojan <james.viojan@accenture.com>
2021-05-10 16:06:29 +0800

    Extract enzyme setup to setupTests.js

--- src/Jotto/Congrats.test.js
+++ src/Jotto/Congrats.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Enzyme, {shallow} from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
+import {shallow} from 'enzyme'
+
 import Congrats from './Congrats'
 import {findByTestAttribute, checkProps} from '../../test/testUtilities'
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
+
 
 const defaultProps = {success : false}
 
